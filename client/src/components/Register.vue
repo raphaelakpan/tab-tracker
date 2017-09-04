10b219f326d5c9a2150a295406a686d1abd7c4e9
Raphael Ifiok Akpan <raphael.akpan@andela.com>
2017-09-04 06:39:33 +0100

    setup user registeration endpoint  - create vue route, component and authentication service

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <form @submit.prevent="register">
+      <div class="form-group">
+        <input type="email" placeholder="Email" name="email" v-model="email">
+      </div>
+      <div class="form-group">
+        <input type="password" placeholder="Password" name="password" v-model="password">
+      </div>
+      <div class="form-group">
+        <button type="submit">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import Authentication from '@/services/Authentication'
+
+  export default {
+    data () {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async register () {
+        const response = await Authentication.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      }
+    }
+  }
+</script>
+
--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'Hola! You\'re welcome'
+    message: 'User registered successfully!'
   })
 })
 
